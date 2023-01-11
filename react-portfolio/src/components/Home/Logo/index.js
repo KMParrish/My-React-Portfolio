306c483569e5e0f1c5b077c264688fcfcf22084f
Kyle Parrish <110362534+KMParrish@users.noreply.github.com>
2023-01-10 18:49:07 -0600

    done

--- react-portfolio/src/components/Home/Logo/index.js
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial'
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import LogoS from '../../../assets/images/k-logo-4.png'
+import LogoS from '../../../assets/images/cover.png'
 import './index.scss'
 
 const Logo = () => {
